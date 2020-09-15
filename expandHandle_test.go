a0b407ae3dfe9c0d5a4b0308cdc7d2a3d393a50c
Doug Saylor <doug@saylorsolutions.com>
2020-09-14 23:14:21 -0500

    Adding test coverage for expand handle

---
+++ expandHandle_test.go
@@ -0,0 +1,69 @@
+package fynetree
+
+import (
+	"fyne.io/fyne"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/test"
+	"github.com/drognisep/fynetree/model"
+	"testing"
+)
+
+type expandHandleModel struct {}
+
+func (e *expandHandleModel) GetIconResource() fyne.Resource {
+	return nil
+}
+
+func (e *expandHandleModel) GetText() string {
+	return ""
+}
+
+func TestNewExpandHandle(t *testing.T) {
+	testApp := test.NewApp()
+	win := testApp.NewWindow("Some Window")
+
+	node := model.NewTreeNode(&expandHandleModel{})
+	node.SetBranch()
+	node.Expand()
+	handle := NewExpandHandle(node)
+
+	if node.IsLeaf() || !node.IsExpanded() {
+		t.Errorf("Expected node to be a branch and expanded. IsLeaf = %v, IsExpanded = %v", node.IsLeaf(), node.IsExpanded())
+	}
+
+	win.SetContent(fyne.NewContainerWithLayout(
+		layout.NewHBoxLayout(),
+		handle,
+	))
+
+	win.ShowAndRun()
+
+	test.Tap(handle)
+	if node.IsExpanded() == true {
+		t.Errorf("Expected node to now be collapsed")
+	}
+}
+
+func TestNewExpandHandleHiddenWhenLeaf(t *testing.T) {
+	testApp := test.NewApp()
+	win := testApp.NewWindow("Some Window")
+
+	node := model.NewTreeNode(&expandHandleModel{})
+	node.SetLeaf()
+	handle := NewExpandHandle(node)
+
+	if !node.IsLeaf() {
+		t.Errorf("Expected node to be a leaf")
+	}
+
+	win.SetContent(fyne.NewContainerWithLayout(
+		layout.NewHBoxLayout(),
+		handle,
+	))
+
+	win.ShowAndRun()
+
+	if handle.Hidden == false {
+		t.Errorf("Expected handle to be hidden if the node is a leaf")
+	}
+}
